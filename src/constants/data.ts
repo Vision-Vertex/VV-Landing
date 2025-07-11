915d7c4484794d52d044b9a3a43352a4d02c9394
abiywondimu5758 <abiywondimu1@gmail.com>
2025-07-11 16:04:43 +0300

    partner image issues handled

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -29,9 +29,10 @@ import commitment from '../../public/qualities/commitment.jpg';
 import thinking from '../../public/qualities/thinking.jpg';
 import Partner1 from '../../public/partners/cropped-output-onlinepngtools-2.webp';
 import Partner2 from '../../public/partners/jem.png';
-import Partner3 from '../../public/partners/photo_2025-06-19 17.40.15.jpeg';
-import Partner4 from '../../public/partners/DailyOfficeSolutions.jpg';
-import Partner5 from '../../public/partners/TIME.jpg';
+import Partner3 from '../../public/partners/tinova.png';
+import Partner4 from '../../public/partners/dailyofficesolutions.png';
+import Partner5 from '../../public/partners/wagms.png';
+import Partner6 from '../../public/partners/TIME.png';
 import { UserRound, Users2Icon, icons } from 'lucide-react';
 import { slugify } from '@/lib/slugify';
 import { FaPaintBrush, FaHandshake, FaRocket, FaUserCheck } from 'react-icons/fa';
@@ -127,8 +128,8 @@ export const partners: PartnerItem[] = [
     alt: 'Partner 5',
   },
   {
-    path: Partner3,
-    alt: 'Partner 3',
+    path: Partner6,
+    alt: 'Partner 6',
   },
   {
     path: Partner1,
@@ -139,8 +140,8 @@ export const partners: PartnerItem[] = [
     alt: 'Partner 2',
   },
   {
-    path: Partner4,
-    alt: 'Partner 5',
+    path: Partner3,
+    alt: 'Partner 3',
   },
  
 ];
