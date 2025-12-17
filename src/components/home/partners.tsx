a2ec6c65283121bf51b25d9a4d0a803559c391f2
betiiy-haile <betelhemhaile74@gmail.com>
2025-12-17 12:20:51 +0300

    minor changes

--- src/components/home/partners.tsx
+++ src/components/home/partners.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 import { motion } from "framer-motion";
 import { ArrowRight, Handshake, Star } from "lucide-react";
 
-const marqueePartners = [...partneritems, ...partneritems];
+// const marqueePartners = [...partneritems, ...partneritems];
 
 function Partners() {
   return (
